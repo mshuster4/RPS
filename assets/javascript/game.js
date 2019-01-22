79fef1092037aca8ee5adde2da77e39d6753c52d
Emroze <emrozesd@users.noreply.github.com>
2019-01-21 20:53:52 -0800

    added firebase info

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,14 @@
-//Players input their names
-//Once both player ready, game begins
-//Players can click on either rock, paper, or scissors
-//Wins, Losses, and Ties are kept track of 
+var config = {
+    apiKey: "AIzaSyDVOnepiOLoGlTYtuN7b7ZR0J_zMs20EvA",
+    authDomain: "rock-paper-scissors-83eba.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-83eba.firebaseio.com",
+    projectId: "rock-paper-scissors-83eba",
+    storageBucket: "rock-paper-scissors-83eba.appspot.com",
+    messagingSenderId: "293978328306"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  var players = database.ref("players");
+  
