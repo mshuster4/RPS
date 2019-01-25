8860382cce1c194e4ee4b8b72df7ef7c8806427f
Emroze <emrozesd@users.noreply.github.com>
2019-01-24 17:57:48 -0800

    Added some logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,91 @@ var config = {
     messagingSenderId: "293978328306"
   };
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  var database = firebase.database();
-  var players = database.ref("players");
-  
+var database = firebase.database();
+var playersRef = database.ref("players");
+var playerOneRef = database.ref("players/one");
+var playerTwoRef = database.ref("players/two");
+var chat = database.ref("chat"); 
+var connectionsRef = database.ref("/connections");
+var isConnectedRef = database.ref(".info/connected");
+
+var numOfPlayers = 0;
+var playerOneName = "";
+var playerTwoName = "";
+var playerOneWins = 0;
+var playerTwoWins = 0;
+var playerOneLoss = 0;
+var playerTwoLoss = 0;
+
+isConnectedRef.on("value", function(snap) {
+
+if (snap.val()) {
+
+        var con = connectionsRef.push(true);
+
+        con.onDisconnect().remove(); 
+
+}
+
+}); 
+
+
+
+$("#name-submit-button").on("click", function() {
+
+    var name = $("#name-input").val().trim(); 
+     $("#name-input").val("");
+
+    connectionsRef.on("value", function(snap) {
+        
+            if (snap.child(1).exists &&) {
+                
+                playersRef.set({
+                    one: {
+                        playerOneName: name,
+                        wins: playerOneWins,
+                        loss: playerOneLoss,
+                    },
+
+                    two: {
+                        playerTwoName: name,
+                        wins: playerTwoWins,
+                        loss: playerTwoLoss,
+
+                });
+
+                waitingForPlayer();
+
+            }
+
+            if ((snap.child(1).exits) && (snap.child(2).exists)) {
+
+                $("#waiting-message").empty();
+
+            }
+
+    });
+
+});
+
+function waitingForPlayer() {
+
+    waitingDiv = $("<div>")
+    waitingDiv.addClass("col-sm-12")
+    waitingDiv.html("<p>" + "Waiting for another player.." + "</p>")
+    $("#waiting-message").append(waitingDiv);
+
+}
+
+
+
+
+
+
+
+
+
+
+
